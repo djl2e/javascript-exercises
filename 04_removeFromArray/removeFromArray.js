84785621cfb3d78b5cd5eb9768b05b726a635b18
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-01-16 23:58:34 +0900

    completed exercises 1-7 of javascript-exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...num) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (num.includes(arr[i])) arr.splice(i, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  temp = (temp - 32) * 5 / 9;
+  return roundToOneDecimal(temp);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  temp = temp * 9 / 5 + 32;
+  return roundToOneDecimal(temp);
 };
+
+function roundToOneDecimal(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
